e377e4f4a46d6596a5a8fe65db0e5c8736fd84cc
tetolalo <jaimeal.99@hotmail.com>
2018-03-21 00:29:12 -0500

    Avance footer y demás secciones

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 //SCROLL REVEAL
 
 window.sr = ScrollReveal();
-sr.reveal('#info');
+sr.reveal('#promo');
+sr.reveal('#carac');
 sr.reveal('#best-of-day');
 sr.reveal('#galery');
 sr.reveal('#main-footer');
