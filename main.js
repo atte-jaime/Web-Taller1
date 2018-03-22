386c57cf902302190aff97e6141ee20cd5592940
tetolalo <jaimeal.99@hotmail.com>
2018-03-22 12:06:18 -0500

    Reproduce canciones correctamente, faltan micro interacciones

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-//SCROLL REVEAL
+//////REPRODUCE
+var canciones = document.querySelectorAll(".cover");
 
+function reproducir(temp) {
+    var audio = temp.getElementsByClassName("song")[0];
+    var boton = temp.querySelector('.play-icon');
+    if (audio.paused == false) {
+        audio.pause();
+        boton.style.backgroundImage = "url('./icons/play.svg')";
+    } else {
+        audio.play();
+        
+        boton.style.backgroundImage = "url('./icons/pause.svg')";
+    }
+}
+
+
+canciones.forEach(element => {
+    element.addEventListener('click', function () {
+        reproducir(element);
+        
+    });
+});
+
+
+
+
+
+//SCROLL REVEAL
 window.sr = ScrollReveal();
 sr.reveal('#promo');
 sr.reveal('#carac');
